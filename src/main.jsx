a4dba29daa0449955a11fcffcfff497a9dcfff36
ascomismep-hue <ascomismep@gmail.com>
2026-07-17 16:46:05 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
-import { AuthProvider } from '@/lib/AuthContext' // Importe o Provider que criamos
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 )
