2cd4c057b9fe42c2b8b51f74a01a081b26f40019
ascomismep-hue <ascomismep@gmail.com>
2026-07-17 17:00:20 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- ISSO É ESSENCIAL para domínios prontos
+  // Isso força o Vite a usar caminhos relativos para scripts e CSS
+  base: './', 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
