e69d5b270a8beb82e0cff75204f5b04cf0fe3ba5
ascomismep-hue <ascomismep@gmail.com>
2026-07-20 20:41:03 -0300

    Update supabaseClient.js

--- src/api/supabaseClient.js
+++ src/api/supabaseClient.js
@@ -1,6 +1,6 @@
-mport { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.'quyaqhapujunxtkibchk.supabase.co'
-const supabaseAnonKey = import.meta.'env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1eWFxaGFwdWp1bnh0a2liY2hrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyOTEyMTMsImV4cCI6MjA5OTg2NzIxM30.n9F00pKSGLHrFR3n5gqpmjoBqQWqhwYLo5EtWPh83xk'
+const supabaseUrl = import.meta.env.'quyaqhapujunxtkibchk.supabase.co';
+const supabaseAnonKey = import.meta.'env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF1eWFxaGFwdWp1bnh0a2liY2hrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQyOTEyMTMsImV4cCI6MjA5OTg2NzIxM30.n9F00pKSGLHrFR3n5gqpmjoBqQWqhwYLo5EtWPh83xk';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
