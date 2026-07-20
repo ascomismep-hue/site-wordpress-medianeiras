5e5d94b855902b39f0eeb702567ef5206ed05260
ascomismep-hue <ascomismep@gmail.com>
2026-07-20 15:17:51 -0300

    Update AdminMensagens.jsx

--- src/pages/admin/AdminMensagens.jsx
+++ src/pages/admin/AdminMensagens.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { supabase } from "@/lib/supabaseClient";
+import { supabase } from "@/api/supabaseClient";
 import { Heart, Mail, Inbox } from "lucide-react";
 
 export default function AdminMensagens() {
