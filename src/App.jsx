5c1cd6c8c421a0e3ca4d210d173227ec150c198e
ascomismep-hue <ascomismep@gmail.com>
2026-07-20 22:23:20 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Router>
+      <div className="flex flex-col min-h-screen bg-[#fcfbf9]">
+        {/* Menu Superior com a Logo Original */}
+        <Header />
+
+        {/* Conteúdo Dinâmico das Rotas */}
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* Outras rotas do site */}
+          </Routes>
+        </main>
+
+        {/* Rodapé com o endereço de Salvador e Ícone */}
+        <Footer />
+      </div>
+    </Router>
   );
 }
