02193bc46c035fef94d7a1c715a48a64de06ae85
ascomismep-hue <ascomismep@gmail.com>
2026-07-21 11:07:11 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import InstitucionalSubNav from "./components/InstitucionalSubNav";
+
+// Import das subpáginas institucionais
+import SobreNos from "./pages/institucional/SobreNos";
+import MadresGerais from "./pages/institucional/MadresGerais";
+import Irmas from "./pages/institucional/Irmas";
+import Memorial from "./pages/institucional/Memorial";
+import CausaDomCampelo from "./pages/institucional/CausaDomCampelo";
+
+function LayoutInstitucional({ children }) {
+  return (
+    <div>
+      <InstitucionalSubNav />
+      {children}
+    </div>
+  );
+}
 
 export default function App() {
   return (
@@ -11,6 +28,13 @@ export default function App() {
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
+            
+            {/* Rotas das Subpáginas Institucionais */}
+            <Route path="/institucional/sobre-nos" element={<LayoutInstitucional><SobreNos /></LayoutInstitucional>} />
+            <Route path="/institucional/madres-gerais" element={<LayoutInstitucional><MadresGerais /></LayoutInstitucional>} />
+            <Route path="/institucional/irmas" element={<LayoutInstitucional><Irmas /></LayoutInstitucional>} />
+            <Route path="/institucional/memorial" element={<LayoutInstitucional><Memorial /></LayoutInstitucional>} />
+            <Route path="/institucional/causa-dom-campelo" element={<LayoutInstitucional><CausaDomCampelo /></LayoutInstitucional>} />
           </Routes>
         </main>
         <Footer />
