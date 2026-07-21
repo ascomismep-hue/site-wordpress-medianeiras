5f29aab8e6ddd96791a618d7b6ca864e125c11fa
ascomismep-hue <ascomismep@gmail.com>
2026-07-20 22:26:02 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,12 @@ export default function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-[#fcfbf9]">
-        {/* Menu Superior com a Logo Original */}
         <Header />
-
-        {/* Conteúdo Dinâmico das Rotas */}
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* Outras rotas do site */}
           </Routes>
         </main>
-
-        {/* Rodapé com o endereço de Salvador e Ícone */}
         <Footer />
       </div>
     </Router>
